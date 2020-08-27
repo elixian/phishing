0eba48c5aa1e542c45d7393552c67a1b8c1666e2
Franck Colombo <franck.colombo@cnav.fr>
2020-08-27 12:54:19 +0200

    modification mail08 => VANC suite retour Thomas

--- src/components/Campaign/Mail08.vue
+++ src/components/Campaign/Mail08.vue
@@ -10,9 +10,9 @@
       <Tooltip infoOverlay="overlay-1" positionSpot="position-1">
         <template v-slot:textinfo>
           L’adresse d’envoi et l’URL de redirection sont cohérentes cependant on
-          remarque qu’il s’agit d’un domaine en .email. Les services légitimes
-          français sont généralement hébergés sur des domaines en
-          <code>.fr</code> ou en <code>.com</code>
+          remarque qu’il s’agit d’un domaine en <code>.email</code> . Les
+          services légitimes français sont généralement hébergés sur des
+          domaines en <code>.fr</code> ou en <code>.com</code>
           (pour l’aspect international). Si vous voyez un domaine d’émission un
           peu particulier, même si ce n’est pas un critère discriminant, cela
           doit attirer votre attention.
@@ -25,7 +25,11 @@
           Même si le sujet est urgent, prenez toujours le temps de la réflexion.
         </template>
       </Tooltip>
-      <Tooltip infoOverlay="overlay-3" positionSpot="position-3" spot-darken=true>
+      <Tooltip
+        infoOverlay="overlay-3"
+        positionSpot="position-3"
+        spot-darken="true"
+      >
         <template v-slot:textinfo>
           Afin d’ajouter de la confiance au mail, les données (N° de facture,
           montant…) sont souvent très précises. Cependant dans les mails
@@ -34,13 +38,26 @@
           informations précises
         </template>
       </Tooltip>
-      <Tooltip infoOverlay="overlay-4" positionSpot="position-4" spot-darken=true>
+      <Tooltip
+        infoOverlay="overlay-4"
+        positionSpot="position-4"
+        spot-darken="true"
+      >
         <template v-slot:textinfo>
           Attention au faux sentiment de sécurité. Pour rendre légitime et
           &laquo;sécurisé&raquo; un mail les pirates informatiques n’hésites pas
           à agréger les multiples logos &laquo;sécurité&raquo; qu’ils peuvent
           trouver. Soyez vigilant, un logo sécurité ne veut pas dire confiance
           aveugle au mail
+        </template>
+      </Tooltip>
+      <Tooltip
+        infoOverlay="overlay-5"
+        positionSpot="position-5"
+        spot-darken="true"
+      >
+        <template v-slot:textinfo>
+          Soyez vigilants aux formulations des mails. Un ton trop familier, des fautes d’orthographe ou des phrases mal formulées sont des indices qui doivent vous alerter.
         </template>
       </Tooltip>
     </div>
@@ -58,7 +75,7 @@ export default {
 <style lang="scss">
 .position-1 {
   top: 5rem;
-  left: 19rem;
+  left: 23rem;
 }
 .position-2 {
   top: 2rem;
@@ -66,31 +83,34 @@ export default {
 }
 
 .position-3 {
-  top: 21rem;
-  left: 42rem;
+  top: 20rem;
+left: 55rem;
 }
 .position-4 {
   top: 37rem;
   left: 44rem;
+}
+.position-5{
+  top: 32rem;
+left: 18rem;
 }
 
 .overlay-1 {
   clip-path: polygon(
     0% 0%,
     0% 100%,
-    22.71% 100%,
-    22.71% 12.77%,
-    22.71% 7.36%,
-    31.68% 7.36%,
-    31.68% 12.77%,
-    22.71% 12.77%,
-    22.71% 100%,
-    63.74% 100%,
-    64% 58.34%,
-    87.24% 58.39%,
-    87.28% 64.44%,
-    63.74% 64.68%,
-    63.74% 100%,
+    27.5% 100%,
+    27.5% 9.5%,
+    34.4% 9.5%,
+    34.4% 14.33%,
+    27.5% 14.33%,
+    27.5% 100%,
+    67.71% 100%,
+    67.71% 62.51%,
+    87.24% 62.51%,
+    87.24% 65.98%,
+    67.71% 65.98%,
+    67.71% 100%,
     100% 100%,
     100% 0%
   );
@@ -107,18 +127,17 @@ export default {
     8.01% 6.78%,
     0.53% 6.79%,
     22.71% 100%,
-    63.74% 100%,
-    42.66% 77.06%,
-    33.23% 77.02%,
-    33.26% 72.75%,
-    34.82% 50.81%,
-    27.86% 50.77%,
-    27.86% 46.71%,
-    34.17% 46.7%,
-    33.18% 72.82%,
-    42.54% 72.82%,
-    42.58% 77.36%,
-    63.74% 100%,
+    38.48% 100%,
+    38.48% 83.83%,
+    31.69% 83.79%,
+    31.69% 51.55%,
+    23.25% 51.55%,
+    23.25% 46.71%,
+    30.17% 46.71%,
+    30.17% 72.82%,
+    30% 79.86%,
+    38.48% 79.97%,
+    38.48% 100%,
     100% 100%,
     100% 0%
   );
@@ -127,19 +146,17 @@ export default {
   clip-path: polygon(
     0% 0%,
     0% 100%,
-    25.71% 100%,
-    25.97% 45.45%,
-    68.3% 45.93%,
-    74.01% 45.93%,
-    74.01% 51.31%,
-    68.16% 51.31%,
-    68.25% 45.7%,
-    58.79% 45.71%,
-    58.81% 51.19%,
-    50.8% 51.07%,
-    50.82% 45.48%,
-    25.97% 45.22%,
-    25.71% 100%,
+    50.56% 100%,
+    50.56% 47.56%,
+    74.01% 47.56%,
+    74.01% 52%,
+    69% 52%,
+    69% 47.26%,
+    58% 47.56%,
+    58% 52%,
+    50.56% 52%,
+    50.56% 47.56%,
+    50.56% 100%,
     100% 100%,
     100% 0%
   );
@@ -148,18 +165,21 @@ export default {
   clip-path: polygon(
     0% 0%,
     0% 100%,
-    40.77% 100%,
-    40.77% 77.31%,
-    62.85% 77.31%,
-    62.85% 88.53%,
-    54.35% 88.53%,
-    54.35% 77.31%,
-    47.41% 77.31%,
-    47.41% 89.08%,
-    40.77% 89.08%,
-    40.77% 100%,
+    39.23% 100%,
+    39.23% 85.13%,
+    60% 85.13%,
+    60% 97.53%,
+    52.8% 97.53%,
+    52.8% 77.31%,
+    45.5% 77.31%,
+    45.5% 97.53%,
+    39.23% 97.53%,
+    39.23% 100%,
     100% 100%,
     100% 0%
   );
+}
+.overlay-5 {
+  clip-path: polygon(0% 0%, 0% 100%, 39.23% 100%, 28.82% 83.34%, 9.16% 83.29%, 9.18% 78.63%, 28.92% 78.75%, 26.82% 55.72%, 26.84% 51.35%, 46.81% 51.57%, 46.83% 55.79%, 26.84% 55.77%, 28.86% 78.57%, 28.83% 85.16%, 39.23% 100%, 100% 100%, 100% 0%);
 }
 </style>
