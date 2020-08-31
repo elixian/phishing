657297d6a1b47ef32219102595b32cfcf3f1e105
Franck Colombo <franck.colombo@cnav.fr>
2020-08-31 17:10:07 +0200

    404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        Hey there !! go away :)
+    <div id="noAccess">
+        <img src="@/assets/images/campaign/Kodama-min.gif" alt="ma" srcset="">
+        <a href="https://fr.wikipedia.org/wiki/Hame%C3%A7onnage"  class="goaway">Next Tree .</a>
     </div>
 </template>
+
+
+
+
+<style lang="scss" scoped>
+
+#noAccess{
+    background-color:#060607;
+    overflow: hidden;
+    position: relative;
+    font-family: 'Gingerbread House',Arial, Helvetica, sans-serif;
+}
+img{
+    width:50%;
+    margin: 0 auto;
+    background-color:#060607;
+}
+.goaway{
+    color:white;
+    position:absolute;
+    top:50%;
+    left:50%;
+    font-size:10rem;
+    text-decoration:none;
+}
+</style>
