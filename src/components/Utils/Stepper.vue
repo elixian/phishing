63eb8e2d10ec3e2dc720facae3c9aca98119a675
Franck Colombo <franck.colombo@cnav.fr>
2020-09-02 15:50:35 +0200

    stepper view and store

--- src/components/Utils/Stepper.vue
+++ src/components/Utils/Stepper.vue
@@ -1,5 +1,37 @@
 <template>
-    <div>
-        Stepper
-    </div>
+  <div>
+    {{totalSteps}}
+    <ul id="stepper">
+      <li v-for="(step,index) in totalSteps" :key='index'></li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import {mapState} from 'vuex';
+export default {
+  
+  computed:{
+    ...mapState('stepper',['totalSteps'])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#stepper{
+    display: flex;
+    width:227px;
+    margin:0;
+    li{
+        width:20px;
+        height:1px;
+        border:none;
+        outline:none;
+        list-style: none;
+        background-color:#C4C4C4;
+        &:not(:first-child){
+          margin-left:3px;
+        }
+    }
+}
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       <h2>Quizz</h2>
+       <hr>
+       <app-stepper/>
+    </div>
+</template>
+
+<script>
+import Stepper from '@/components/Utils/Stepper';
+export default {
+    components:{
+        appStepper : Stepper
+    }
+}
+</script>
