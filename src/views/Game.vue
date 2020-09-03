8239c47f1927bcf6962926950907efcd3fff3ee8
Franck Colombo <franck.colombo@cnav.fr>
2020-09-03 10:09:46 +0200

    modif gitignore

---
+++ src/views/Game.vue
@@ -0,0 +1,24 @@
+yarn<template>
+    <div>
+        <div id="logo-quizz">
+            <img src="@/assets/images/game/LOGO-quizz.png" alt="logo Quizz" srcset="">
+        </div>
+       <h2>Quizz</h2>
+       <hr>
+       <app-stepper/>
+    </div>
+</template>
+
+<script>
+import Stepper from '@/components/Utils/Stepper';
+export default {
+    components:{
+        appStepper : Stepper
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    
+</style>
