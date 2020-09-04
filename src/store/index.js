51ef616b61e1d46e292a654e370204ebe42b1d1c
Franck Colombo <franck.colombo@cnav.fr>
2020-09-04 13:56:37 +0200

    game store

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import presentation from './module/presentation';
 import stepper from './module/stepper';
+import game from './module/game.js';
+
 Vue.use(Vuex)
 
 
 
 export default new Vuex.Store({
   modules: {
+    game,
     presentation,
-    stepper
+    stepper,
   },
   state:{
 
---
+++ src/store/module/game.js
@@ -0,0 +1,25 @@
+export default{
+    namespaced: true,
+    state:{
+        gameIsStarted:false,
+        currentGame:{
+            isPhishing:null
+        }
+    },
+    mutations:{
+        SET_CURRENT_STATUS_PHISHING(state,isPhishing){
+            state.currentGame.isPhishing = isPhishing;
+        },
+        SET_GAME_STARTED(state){
+            state.gameIsStarted = true;
+        }
+    },
+    actions:{
+        setCurrentStatus({commit},isPhishing){
+            commit('SET_CURRENT_STATUS_PHISHING',isPhishing);
+        },
+        startGame({commit}){
+            commit('SET_GAME_STARTED');
+        }
+    },   
+}
