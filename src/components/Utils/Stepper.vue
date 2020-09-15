3df4f5e0853658209d0f8888df811fd2005e3884
Franck Colombo <franck.colombo@cnav.fr>
2020-09-15 07:47:20 +0200

    modificationb du stepper

--- src/components/Utils/Stepper.vue
+++ src/components/Utils/Stepper.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
 
-    <div class="step2" :style="{width:percentStage+'%'}"> </div>
+    <div id="stepper" :style="{width:percentStage+'%'}"> </div>
   </div>
 </template>
 
@@ -17,14 +17,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.step2{
+#stepper{
   position:absolute;
   top:0;
   left:0;
   height: 2px;
   width: 0%;
   background:#0C7193;
-  transition: width 1s;
+  transition: width 1s ease-out;
 }
 
 @keyframes fluidStep{
